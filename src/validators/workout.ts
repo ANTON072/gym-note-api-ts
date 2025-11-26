3726ea84b2fc6c860a1d85f8d4d1398e54a26a0c
Katsushi OUGI <ougi@to-r.net>
2025-11-26 22:13:43 +0900

    feat: update workout validation schemas to include request types and omit unnecessary fields

--- src/validators/workout.ts
+++ src/validators/workout.ts
@@ -7,12 +7,25 @@ export const workoutSetSchema = z.object({
   reps: z.number().int().min(0).nullable(),
 });
 
+export const workoutSetRequestSchema = workoutSetSchema.omit({ id: true });
+
 export const workoutExerciseSchema = z.object({
   id: z.string(),
   orderIndex: z.number().int().min(0),
   exercise: exerciseSchema.omit({ createdAt: true, updatedAt: true }),
   workoutSets: z.array(workoutSetSchema),
 });
+
+export const workoutExerciseRequestSchema = workoutExerciseSchema
+  .omit({
+    id: true,
+    exercise: true,
+    workoutSets: true,
+  })
+  .extend({
+    exerciseId: z.string(),
+    workoutSets: z.array(workoutSetRequestSchema),
+  });
 
 export const workoutSchema = z.object({
   id: z.string(),
@@ -25,3 +38,24 @@ export const workoutSchema = z.object({
   deletedAt: z.date().nullable(),
   workoutExercises: z.array(workoutExerciseSchema),
 });
+
+export const workoutRequestSchema = workoutSchema
+  .omit({
+    id: true,
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    workoutExercises: true,
+  })
+  .extend({
+    workoutExercises: z.array(workoutExerciseRequestSchema),
+  });
+
+export type Workout = z.infer<typeof workoutSchema>;
+export type WorkoutRequest = z.infer<typeof workoutRequestSchema>;
+export type WorkoutExercise = z.infer<typeof workoutExerciseSchema>;
+export type WorkoutExerciseRequest = z.infer<
+  typeof workoutExerciseRequestSchema
+>;
+export type WorkoutSet = z.infer<typeof workoutSetSchema>;
+export type WorkoutSetRequest = z.infer<typeof workoutSetRequestSchema>;
